7d522ca1e6bd2538e87a0257c3adb02d13bd39b4
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-01-18 15:27:29 -0800

    fix the position of components

--- src/components/Buttons/UploadButton.js
+++ src/components/Buttons/UploadButton.js
@@ -2,7 +2,7 @@ import "./Button.scss";
 import icon from "../../assets/Icons/upload.svg";
 const UploadButton = () => {
   return (
-    <button className="button">
+    <button className="button upload__button">
       <div className="button__div-icon">
       <img className="button__icon" src={icon}></img>
       </div>
